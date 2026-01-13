b481f69d56be6ddd47a340f8c79ae1a1ba4a2b5f
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-14 00:16:17 +0200

    set title of MealsOverviewScreen

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,13 +1,17 @@
 import { RouteProp } from '@react-navigation/native';
 import { FlatList, View, StyleSheet, ListRenderItemInfo } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
 import Meal from '../models/meal';
 import MealItem from '../components/MealItem';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useLayoutEffect } from 'react';
 
 const MealsOverviewScreen = ({
   route,
+  navigation,
 }: {
   route?: RouteProp<{ MealsOverview: { categoryId: string } }>;
+  navigation?: NativeStackNavigationProp<any>;
 }) => {
   const categoryId = route?.params?.categoryId ?? '';
 
@@ -18,6 +22,15 @@ const MealsOverviewScreen = ({
   const renderMealItem = (itemData: ListRenderItemInfo<Meal>) => {
     return <MealItem {...itemData.item} />;
   };
+
+  useLayoutEffect(() => {
+    const categoryTitle =
+      CATEGORIES.find(category => category.id === categoryId)?.title ?? '';
+
+    navigation?.setOptions({
+      title: categoryTitle,
+    });
+  }, [navigation, categoryId]);
   return (
     <View style={styles.container}>
       <FlatList
