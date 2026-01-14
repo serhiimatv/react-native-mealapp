69c976d9d3919251fe08ac1921f633789a51673a
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2026-01-14 22:43:55 +0200

    code for header button

--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,18 +1,43 @@
 import { RouteProp } from '@react-navigation/native';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from 'react-native';
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
+import { useLayoutEffect } from 'react';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const MealDetailScreen = ({
   route,
+  navigation,
 }: {
   route?: RouteProp<{ MealDetail: { mealId: string } }>;
+  navigation?: NativeStackNavigationProp<{ MealDetail: { mealId: string } }>;
 }) => {
   const mealId = route?.params?.mealId ?? '';
 
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  const headerButtonPressHandler = () => {
+    console.log('Button pressed!');
+  };
+
+  useLayoutEffect(() => {
+    navigation?.setOptions({
+      title: selectedMeal?.title,
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerRight: () => {
+        return <Button title="Tap me" onPress={headerButtonPressHandler} />;
+      },
+    });
+  }, [navigation, mealId, selectedMeal]);
   return (
     <ScrollView style={styles.rootScreen}>
       <Image style={styles.image} source={{ uri: selectedMeal?.imageUrl }} />
